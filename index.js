476577326b1770288384b17da8280806dfc062e1
David Garcia <davidgar026@gmail.com>
2024-12-15 17:32:37 -0500

    commit

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "pimpin",
+  port: 5432
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,7 +22,17 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+async function getItems(){
+  const result = await db.query("SELECT * FROM items")
+  items = result.rows;
+  console.log("items = ", items)
+  return items;
+}
+
+
+app.get("/", async (req, res) => {
+
+  await getItems();
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
@@ -25,9 +45,17 @@ app.post("/add", (req, res) => {
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", (req, res) => {
 
-app.post("/delete", (req, res) => {});
+  res.redirect("/")
+});
+
+app.post("/delete", async (req, res) => {
+  console.log("req.body.deleteItemId = ", req.body.deleteItemId)
+  await db.query("DELETE FROM items WHERE id = $1", [req.body.deleteItemId]);
+  await getItems();
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
